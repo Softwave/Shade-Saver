cd4763de67a5a5619b5e816cbb65851a7d72c513
Serene Leyba <development@softwavestudios.com>
2024-02-02 02:49:43 -0500

    Updated

--- main.cpp
+++ main.cpp
@@ -5,44 +5,41 @@
 #include <QMenu>
 
 
-class PigmentPicker : public QApplication
-{
-public:
-    MainWindow w;
-    PigmentPicker(int argc, char *argv[]) : QApplication(argc, argv)
-    {
-        // Create the system tray icon
-        QSystemTrayIcon* trayIcon = new QSystemTrayIcon(QIcon(":/new/prefix1/ColPicker.png"), this);
-        //":/icons/pigment.png"
-        trayIcon->setToolTip("Shade Saver: Double click to show/hide");
-        trayIcon->show();
 
-        // Create the menu
-        QMenu* trayMenu = new QMenu();
-
-        // Connect to new colour in main window
-        trayMenu->addAction("New Color", &w, SLOT(on_actionAdd_New_Color_triggered()));
-
-        trayMenu->addAction("Exit", this, SLOT(quit()));
-
-        trayIcon->setContextMenu(trayMenu);
-
-        // Clicking on the tray icon will show/hide the main window
-        connect(trayIcon, &QSystemTrayIcon::activated, [this](auto reason)
-        {
-            if (reason == QSystemTrayIcon::DoubleClick)
-            {
-                w.show_hide();
-            }
-        });
-    }
-};
 
 int main(int argc, char *argv[])
 {
-    PigmentPicker a(argc, argv);
+    //PigmentPicker a(argc, argv);
+    QApplication app(argc, argv);
+    MainWindow w;
+
+    // Create the system tray icon
+    QSystemTrayIcon* trayIcon = new QSystemTrayIcon(QIcon(":/new/prefix1/ColPicker.png"));
+    //":/icons/pigment.png"
+    trayIcon->setToolTip("Shade Saver: Double click to show/hide");
+    trayIcon->show();
+
+    // Create the menu
+    QMenu* trayMenu = new QMenu();
+
+    // Connect to new colour in main window
+    trayMenu->addAction("New Color", &w, SLOT(on_actionAdd_New_Color_triggered()));
+
+    trayMenu->addAction("Exit", &app, SLOT(quit()));
+
+    trayIcon->setContextMenu(trayMenu);
+
+    // Clicking on the tray icon will show/hide the main window
+    app.connect(trayIcon, &QSystemTrayIcon::activated, [&w](auto reason)
+                {
+                    if (reason == QSystemTrayIcon::Trigger)
+                    {
+                        //w.show_hide();
+                        w.show_hide();
+                    }
+                });
 
 
-    a.w.show();
-    return a.exec();
+    w.show();
+    return app.exec();
 }
